35ba4e496dfd9d71a12a2bee3dcb94d0ece62d25
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-12-03 14:33:50 +0200

    Update key.sh

--- key.sh
+++ key.sh
@@ -4,7 +4,7 @@
 $key = tr -dc 'a-f0-9' < /dev/urandom | head -c32
 $zet = $?
 az keyvault create --name $key --resource-group default --location westeu --enabled-for-disk-encryption
-if [$zet -ne 0] then;
+if [$zet -ne 0]; then
  echo "Error cannot allow to create a key..."
 else
  echo " Key was create with succes..."
